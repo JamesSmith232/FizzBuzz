0ab81df1fe9a816a180038ae4251be088166b425
JamesSmith232 <80684500+JamesSmith232@users.noreply.github.com>
2022-05-26 09:45:16 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -22,12 +22,14 @@ public class FizzBuzz {
         System.out.println("FizzBuzz is a list of numbers that change depending on if the number is a multiple of 3, 5, or both");
         System.out.println("==================================================================================================================\n");
         
+        //Getting the range 
         System.out.println("What is the number you want to start at: ");
         start = keyboard.nextInt();  
         System.out.println("What is the number you want to end at: ");
         end = keyboard.nextInt();
         System.out.println(""); 
         
+        //Calling calcFizzBuzz() in a for loop to display the list of FizzBuzz numbers
         for (int i = start; i <= end; i++) {
             System.out.println("\t\t\t\t\t\t" + i + "  =  "+ calcFizzBuzz(i));
         }
@@ -35,10 +37,12 @@ public class FizzBuzz {
     
     
     public static boolean isDivisble(int number, int divisor) {
+        //returning true or false based on this condition 
         return number % divisor == 0;
     }
 
     public static String calcFizzBuzz(int num) {
+        //if statements to see if the numbers are FizzBuzz or not  
         if(isDivisble(num, THREE) && isDivisble(num, FIVE)) {
             return "FizzBuzz";
         } 
